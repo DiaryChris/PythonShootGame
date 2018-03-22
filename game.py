e052285683e0a5e258abc85e1f86fdaf848aaf55
DIARY-PC\程达瑞 <383001400@qq.com>
2018-03-22 17:58:25 +0800

    add resourse directory

---
+++ game.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import pygame
+from pygame.locals import *
+import random
+
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+
+pygame.init()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
